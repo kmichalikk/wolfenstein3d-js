5ddc0f685c6cc9e1edecc49abfdf4586714d0752
Konrad Michalik <kmichalikk19@gmail.com>
2021-11-27 20:32:05 +0100

    Move texture adjustment to base class

--- src/game/AI/Dog.ts
+++ src/game/AI/Dog.ts
@@ -1,8 +1,6 @@
 import { Vec2, CollisionInfo, BaseEnemy, EnemyType } from "../../utils";
 //@ts-ignore
 import Texture from '../gfx/dogtex.png';
-//@ts-ignore
-import mappings from '../gfx/enemy_mappings.json';
 
 export default class Dog extends BaseEnemy {
 	constructor(pos: Vec2, rot: Vec2) {
@@ -19,28 +17,6 @@ export default class Dog extends BaseEnemy {
 			this.rotation.rotate(0.1);
 			this.nextDecisionCooldown = 4;
 		}
-
-		let vec = this.position.subtractVec(playerPos);
-		let angle = Math.atan2(vec.y, vec.x)
-		if (angle < 0) angle += 2 * Math.PI;
-		angle -= Math.atan2(this.rotation.y, this.rotation.x) + Math.PI;
-		if (angle < 0) angle += 2 * Math.PI;
-
-		if (angle < Math.PI * 0.125 || angle >= Math.PI * 1.875)
-			this.texCoords = mappings["stand-front"];
-		else if (angle < Math.PI * 0.375 && angle >= Math.PI * 0.125)
-			this.texCoords = mappings["stand-front-right"];
-		else if (angle < Math.PI * 0.625 && angle >= Math.PI * 0.375)
-			this.texCoords = mappings["stand-right"];
-		else if (angle < Math.PI * 0.875 && angle >= Math.PI * 0.625)
-			this.texCoords = mappings["stand-back-right"];
-		else if (angle < Math.PI * 1.125 && angle >= Math.PI * 0.875)
-			this.texCoords = mappings["stand-back"];
-		else if (angle < Math.PI * 1.375 && angle >= Math.PI * 1.125)
-			this.texCoords = mappings["stand-back-left"];
-		else if (angle < Math.PI * 1.625 && angle >= Math.PI * 1.375)
-			this.texCoords = mappings["stand-left"];
-		else if (angle < Math.PI * 1.875 && angle >= Math.PI * 1.625)
-			this.texCoords = mappings["stand-front-left"];
+		this.adjustTexture(playerPos, "stand");
 	}
 }
